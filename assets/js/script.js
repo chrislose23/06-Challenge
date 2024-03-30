e37570a416d7e7c07351e86393a73c986d4e7eb5
chrislose23 <chris@Christophers-MacBook-Air.local>
2024-03-30 19:46:26 -0400

    started javascript and console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 const apiKey = '5842a6da3b2ee4a199a3f215f476fb35';
+
+function getWeather(event) {
+    // This prevents defult form submission
+    event.preventDefult();
+
+    const cityName = document.getElementById('searchBar').value;
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+    console.log(cityName);
+    console.log(apiUrl);
+}
