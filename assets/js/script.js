d9ce311b8579caf58a0b6e254e80335e0bfbcf1d
chrislose23 <chris@Christophers-MacBook-Air.local>
2024-03-30 20:02:06 -0400

    fixed javascript to allow search

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,10 @@ const apiKey = '5842a6da3b2ee4a199a3f215f476fb35';
 
 function getWeather(event) {
     // This prevents defult form submission
-    event.preventDefult();
-
+    event.preventDefault();
+    
     const cityName = document.getElementById('searchBar').value;
     const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-
-    console.log(cityName);
-    console.log(apiUrl);
 
     fetch(apiUrl)
         .then(response => {
@@ -31,11 +28,10 @@ function getWeather(event) {
 
             document.getElementById('activeCity').innerHTML = weatherInfo;
         })
-        
         .catch(error => {
             console.error('Error fetching data:', error);
             document.getElementById('activeCity').innerHTML = `<p>Error fetching data: ${error.message}</p>`;
         });
 }
 
-document.getElementById('searchBtn').addEventListener('Click', getWeather);
+document.getElementById('searchBtn').addEventListener('click', getWeather);
