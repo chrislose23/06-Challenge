132aafcac1bc1a6a051992a0ece2eb0cbcdc3b12
chrislose23 <chris@Christophers-MacBook-Air.local>
2024-04-01 14:21:47 -0400

    fixed date to show as intended

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ function getWeather(event) {
             document.getElementById('activeCity').innerHTML = `<p>Error fetching current weather data: ${error.message}</p>`;
         });
 
-    // Fetch 5-day forecast data
+    // Fetch the 5-day forecast data
     fetch(forecastUrl)
     .then(response => {
         if (!response.ok) {
@@ -49,18 +49,21 @@ function getWeather(event) {
     })
     .then(data => {
 
-        // Get unique dates from the forecast data
-        const uniqueDates = [...new Set(data.list.map(item => item.dt_txt.substr(0, 10)))];
-        
-        // Take the first 5 unique dates
-        const forecastData = data.list.filter(item => uniqueDates.includes(item.dt_txt.substr(0, 10))).slice(0, 5);
- 
-        
-        // const forecastData = data.list.slice(0, 5);
+        // I sorted forecast data to include only entries at 3:00 PM
+        const forecastData = data.list.filter(item => {
+            
+            const localHour = new Date(item.dt_txt + ' UTC').getUTCHours();
+            const localMinute = new Date(item.dt_txt + ' UTC').getUTCMinutes();
+            // Check  local time to compare to 3:00 PM
+            return localHour === 15 && localMinute === 0;
+        // Take the first 5 entries only
+        }).slice(0, 5);
+
         let forecastHTML = '';
 
         forecastData.forEach(item => {
             const date = new Date(item.dt_txt);
+            const newDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
             const time = date.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
             const description = item.weather[0].description;
             const temperatureC = item.main.temp;
@@ -71,11 +74,10 @@ function getWeather(event) {
 
             forecastHTML += `
                 <div class="fiveDay">
-                    <p>Date: ${date.toDateString()}</p>
-                    <p>Time: ${time}</p>
-                    <p>Description: ${description}</p>
-                    <p>Temperature: ${temperatureF}°F</p>
-                    <p>Wind Speed: ${windSpeedMPH} m/h</p>
+                    <p>${newDate}</p>
+                    <p>${description}</p>
+                    <p>Temp: ${temperatureF}°F</p>
+                    <p>Wind: ${windSpeedMPH} m/h</p>
                     <p>Humidity: ${humidity}%</p>
                 </div>
             `;
